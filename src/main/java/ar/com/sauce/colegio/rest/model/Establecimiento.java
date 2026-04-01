3aa9dfa5dae7f8c3226d17588d0b0c42a81e7933
almaeq <alma.quinterosquiroga@gmail.com>
2026-04-01 11:30:06 -0300

    add establecimiento to models

---
+++ src/main/java/ar/com/sauce/colegio/rest/model/Establecimiento.java
@@ -0,0 +1,40 @@
+package ar.com.sauce.colegio.rest.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "conf_establecimiento")
+public class Establecimiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_establecimiento")
+    private Long idEstablecimiento;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "nombre_corto")
+    private String nombreCorto;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "fax")
+    private String fax;
+
+    @Column(name = "leyenda")
+    private String leyenda;
+
+    @Column(name = "recargo_diario")
+    private BigDecimal recargoDiario;
+}
